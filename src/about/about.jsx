4b50da8c9e801f8be97761a387a3f4f925dc788f
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-05-26 00:51:02 -0300

    includes component about and todo

---
+++ src/about/about.jsx
@@ -0,0 +1,7 @@
+import React from 'react' // function component
+
+export default props => (
+    <div>
+        <h1>Sobre</h1>
+    </div>
+)
---
+++ src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react' // class component
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
