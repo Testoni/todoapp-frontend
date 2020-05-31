502c3a139c703a6b357808cd8e303c25822ef1cf
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-05-30 23:35:45 -0300

    routes config, menu component, if component, css component

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react' // function component
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        
+        <h2>Nossa História</h2>
+        <p>teste teste teste</p>
+
+        <h2>Teste Missao e Visão</h2>
+        <p>teste teste teste</p>
     </div>
 )
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -2,14 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import Todo from '../todo/todo'
-import Aboout from '../about/about'
 import Menu from '../template/menu'
+import Routes from './routes'
 
 export default props => (
     <div className='container'>
         <Menu />
-        <Todo />
-        <Aboout />
+        <Routes />
     </div>
 )
