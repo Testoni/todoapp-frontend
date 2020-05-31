47b711c1849e598579c758aaa479312546fe4303
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-05-31 03:21:28 -0300

    handleremove, asdone and pending, formlist actions

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -15,6 +15,8 @@ export default class Todo extends Component {
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
 
         this.refresh()
     }
@@ -34,15 +36,30 @@ export default class Todo extends Component {
     }
 
     handleRemove(todo) {
-        axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh()
+        axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh())
+    }
+
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true }).then(resp => this.refresh())
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false }).then(resp => this.refresh())
     }
 
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm handleChange={this.handleChange} description={this.state.description} handleAdd={this.handleAdd} />
-                <TodoList list={this.state.list} handleRemove={this.handleRemove} />
+                <TodoForm 
+                    handleChange={this.handleChange} 
+                    description={this.state.description} 
+                    handleAdd={this.handleAdd} />
+                <TodoList 
+                    list={this.state.list} 
+                    handleRemove={this.handleRemove}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending} />
             </div>
         )
     }
